39889d2de45e3675d4df95f4b78af021dfead160
FelippeSantuzzi <felippesantuzzi@gmail.com>
2026-03-19 18:43:30 -0300

    feat: estrutura inicial do objeto Produto

---
+++ models/produto.py
@@ -0,0 +1,11 @@
+# models/produto.py
+
+class Produto:
+    def __init__(self, nome, preco_compra, preco_venda, data_compra, data_vencimento, quantidade):
+        self.nome = nome
+        self.preco_compra = preco_compra
+        self.preco_venda = preco_venda
+        self.data_compra = data_compra
+        self.data_vencimento = data_vencimento
+        self.quantidade = quantidade
+        self.proximo = None  # Este é o "ponteiro" para o próximo vagão (uma estrutura manual)
